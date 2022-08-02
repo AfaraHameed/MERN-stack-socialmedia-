1fc4e86e84e10727b9d9b29969e251200ee6e802
Afara <afarasidheek@gmail.com>
2022-08-02 12:31:21 +0400

    commit

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -7,7 +7,7 @@ function Feed() {
   const [post, setPost] = useState([])
   useEffect(() => {
    const fetchPost = async ()=>{
-    const res = await axios.get("http://localhost:8800/post/timeline/62c04137586c325564cc3516")
+    const res = await axios.get("/post/timeline/62c04137586c325564cc3516")
     console.log(res.data)
     //setPosts(res.data)
    }
